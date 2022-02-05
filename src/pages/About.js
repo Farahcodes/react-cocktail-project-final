b2a9492386dcf6936347b2815f1ccd382c67e8b7
farah <farah.fournat@gmail.com>
2022-02-05 14:34:21 +0100

    final file

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error, necessitatibus? Eos excepturi ipsam animi odio, quasi dicta similique facilis nihil voluptatibus omnis veritatis odit architecto vero maxime? Debitis cumque explicabo, ex delectus consequuntur error accusantium vitae est suscipit iusto ipsa.</p>
+    </section>
+  )
+}
+
+export default About
